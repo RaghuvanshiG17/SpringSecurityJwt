b771f8d76db365efad9a277f9b271f5255338de9
Ankit_Kumar <Ankit_Kumar@EPINHYDW0396.princeton.epam.com>
2019-04-03 12:17:57 +0530

    SpringSecurity with JWT first commit

---
+++ src/main/java/com/epam/controller/TokenController.java
@@ -0,0 +1,29 @@
+package com.epam.controller;
+
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.epam.model.JwtUser;
+import com.epam.security.JwtGenerator;
+
+@RestController
+@RequestMapping("/token")
+public class TokenController {
+
+
+    private JwtGenerator jwtGenerator;
+
+    public TokenController(JwtGenerator jwtGenerator) {
+        this.jwtGenerator = jwtGenerator;
+    }
+
+    @PostMapping
+    public String generate(@RequestBody final JwtUser jwtUser) {
+
+        return jwtGenerator.generate(jwtUser);
+
+    }
+}
